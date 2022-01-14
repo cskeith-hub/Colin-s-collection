5ed8e2bb5a4569d6e00e3a4248f3c5fbcbf7f9d5
Rustiek <cskeith01@gmail.com>
2022-01-14 21:27:07 +0100

    Add Description's

--- collection.js
+++ collection.js
@@ -27,7 +27,7 @@ const collection = [
         releaseYear: '23 november 2004',
         picture: 'https://images.blz-contentstack.com/v3/assets/blt3452e3b114fab0cd/blt0d3f8b6a37a99e8d/601d9493912a490c07af5702/WoW_Ragnaros_3840x2160_enUS.jpg?auto=webp&quality=75',
         genre: ['MMO-RPG'],
-        description: '<a href="https://worldofwarcraft.com/en-us/">Coming Soon</a>',
+        description: 'The leader of the Horde, Thrall, has settled in the land of Durotar, where he asks the Forsaken to join the Orcs, Tauren and the Trolls. Meanwhile, Dwarves, Gnomes and the ancient Night Elves have joined the Alliance, led by the realm of Humans in Stormwind.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -39,7 +39,7 @@ const collection = [
         releaseYear: '29 oktober 2003',
         picture: 'https://sm.ign.com/ign_nl/screenshot/default/call-of-duty-modern-warfare-twitter-facebook-instagram-going_x817.jpg',
         genre: 'FPS',
-        description: '<a href="https://www.callofduty.com/be/nl/">Coming Soon</a>',
+        description: 'Call of Duty is a series of computer games developed by Infinity Ward, Treyarch and Sledgehammer Games. After the success of the first game, more games followed',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -51,7 +51,7 @@ const collection = [
         releaseYear: '7 juli 2015',
         picture: 'https://www.internetmatters.org/wp-content/uploads/2020/11/Rocket-league-main-image.png',
         genre: 'Multiplayer',
-        description: '<a href="https://www.rocketleague.com/">Coming Soon</a>',
+        description: 'Rocket League is a football-like computer game played with in-game vehicles. Rocket League is developed and published by Psyonix. In the game, players drive a vehicle with which they must try to score goals on a field that resembles a football field.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -61,9 +61,9 @@ const collection = [
         name: 'League of Legends',
         developer: 'Riot Games',
         releaseYear: '27 oktober 2009',
-        picture: 'https://www.gamingnation.nl/wp-content/uploads/2019/10/league-of-legends.jpg',
+        picture: 'https://static.posters.cz/image/1300/posters/league-of-legends-champions-i102252.jpg',
         genre: 'Battle Arena',
-        description: '<a href="https://www.leagueoflegends.com/en-gb/">Coming Soon</a>',
+        description: 'League of Legends is een computerspel ontwikkeld en uitgebracht door Riot Games in 2009. Het spel is gebaseerd op een modificatie van het spel Warcraft III. League of Legends is compatibel met het besturingssysteem Microsoft Windows en sinds 2013 ook met Mac OS X.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -75,7 +75,7 @@ const collection = [
         releaseYear: '2 juni 2015',
         picture: 'https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_ARKSurvivalEvolved.jpg',
         genre: 'Survival Game',
-        description: '<a href="https://ark.fandom.com/wiki/ARK_Survival_Evolved_Wiki">Coming Soon</a>',
+        description: 'Translated from English-Ark: Survival Evolved is a 2017 action-adventure video game developed by Studio Wildcard. In the game, players must survive on an island filled with roaming dinosaurs and other prehistoric animals, natural hazards, and potentially hostile human players.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -87,7 +87,7 @@ const collection = [
         releaseYear: '3 mei 2016',
         picture: 'https://hwimg.nl/Overwatch.png',
         genre: 'FPS',
-        description: '<a href="https://playoverwatch.com/en-us/">Coming Soon</a>',
+        description: 'Overwatch is a multiplayer first-person shooter developed and published by Blizzard Entertainment. The game was released on May 24, 2016 for PlayStation 4, Windows, and Xbox One, and for Nintendo Switch on October 15, 2019.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -99,7 +99,7 @@ const collection = [
         releaseYear: '12 september 2013',
         picture: 'https://hwimg.nl/ARMA-III.jpg',
         genre: 'FPS',
-        description: '<a href="https://arma3.com/">Coming Soon</a>',
+        description: 'ARMA 3 is a military simulation game that can be played in first-person and third-person. The game is developed by Bohemia Interactive for Microsoft Windows',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -111,7 +111,7 @@ const collection = [
         releaseYear: '11 maart 2014',
         picture: 'https://www.gamekings.tv/wp-content/uploads/nieuws20150609_hearthstone.jpg',
         genre: 'Card Game',
-        description: '<a href="https://playhearthstone.com/en-us">Coming Soon</a>',
+        description: 'Hearthstone: Heroes of Warcraft is a digital collectible card game based on the World of Warcraft TCG set from the Warcraft Universe. It was announced on March 22, 2013 during PAX East 2013 by Rob Pardo.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -123,7 +123,7 @@ const collection = [
         releaseYear: '4 januari 2001',
         picture: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1664831/capsule_616x353.jpg?t=1627293872',
         genre: 'MMO-RPG',
-        description: '<a href="https://play.runescape.com/s">Coming Soon</a>',
+        description: 'RuneScape is an MMORPG, by Jagex Ltd, designed by Andrew Gower and released on January 4, 2001. The fantasy game is set on the planet Gielinor where there are "gods" and "monsters". Players can trade, chat, fight or play mini-games with each other. The game is written in C++.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     },
@@ -135,7 +135,7 @@ const collection = [
         releaseYear: '27 juli 2010',
         picture: 'https://pyre-media-2.s3.sa-east-1.amazonaws.com/cb5421ca-fe51-41e2-96bd-514df7f59e37maxresdefault%20(6).jpg',
         genre: 'RTS',
-        description: '<a href="https://starcraft2.com/en-us/">Coming Soon</a>',
+        description: 'StarCraft II is a real-time strategy computer game developed by Blizzard Entertainment. StarCraft II is the sequel to the popular StarCraft. It was announced on May 19, 2007 at the Blizzard Worldwide Invitational in Seoul, South Korea. StarCraft II was released on July 27, 2010 for Windows and Mac OS X.',
         link: '<a href="https://worldofwarcraft.com/en-us/">Visit The World</a>'
 
     }
